c47a810438e6d09fdc4b1b19cf2efcc9feb61eb1
Justin Zablocki <zablockijj@gmail.com>
2018-10-10 19:07:53 -0400

    Create database seeder with teams and players

--- database/factories/TeamFactory.php
+++ database/factories/TeamFactory.php
@@ -3,7 +3,24 @@
 use Faker\Generator as Faker;
 
 $factory->define(\App\Team::class, function (Faker $faker) {
+    $animals = [
+        'Beavers',
+        'Wildcats',
+        'Badgers',
+        'Bats',
+        'Bulldogs',
+        'Zebras',
+        'Goldfish',
+        'Pythons',
+        'Falcons',
+        'Crayfish',
+        'Buffalos',
+        'Yaks',
+        'Crocodiles',
+        'Panthers',
+    ];
+
     return [
-        'name' => $faker->name,
+        'name' => "{$faker->city} {$faker->randomElement($animals)}",
     ];
 });
