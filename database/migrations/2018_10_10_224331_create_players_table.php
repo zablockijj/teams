379af29a3a2ebf3448faa778c819078cdfaa7a2b
Justin Zablocki <zablockijj@gmail.com>
2018-10-10 18:46:37 -0400

    Create initial team and player tables

---
+++ database/migrations/2018_10_10_224331_create_players_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('team_id')->index();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
